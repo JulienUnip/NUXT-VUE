edf1c1d6c0afd0897f7dfdb881fdbb82b05ff000
julien93140 <julien.moratalla@gmail.com>
2019-07-20 16:44:51 +0200

    get request nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
   modules: [
     // Doc: https://bootstrap-vue.js.org/docs/
     'bootstrap-vue/nuxt',
+    '@nuxtjs/axios',
   ],
   /*
   ** Build configuration
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
+
 <template>
   <div>
     <h1>Events</h1>
+
+    <div v-for="(value) in titles" :key="value">
+      {{ value.title }}
+    </div>
+
   </div>
 </template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    asyncData ({ params }) {
+      return axios.get(`https://jsonplaceholder.typicode.com/todos/`)
+      .then((res) => {
+        console.log(res)
+        return { titles: res.data }
+      })
+    }
+  }
+</script>
